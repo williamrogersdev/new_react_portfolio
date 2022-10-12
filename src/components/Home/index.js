b2d1ae67b9ff409110918c8054b9c01723b4a885
William Rogers <williamrogersdev@gmail.com>
2022-10-12 10:47:32 -0500

    home fix

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,20 @@
 import { useEffect, useState } from 'react'
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 import { Link } from 'react-router-dom'
 // import LogoTitle from '../../assets/images/logo-s.png'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
-import Logo from './Logo'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import Logo from './Logo'
+
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -82,7 +92,30 @@ const Home = () => {
             CONTACT ME
           </Link>
         </div>
-        <Logo />
+        {/* <Logo /> */}
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
       </div>
       <Loader type="pacman" />
     </>
