696939c1feed9ec10bed4b2fd5a8a3856a055442
William Rogers <williamrogersdev@gmail.com>
2022-10-12 22:37:56 -0500

    text fix

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -33,18 +33,18 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm a very ambitious front-end developer looking for a role in an
-            established IT company with the opportunity to work with the latest
-            technologies on challenging and diverse projects.
+            I'm a very ambitious fullstack developer ooking for a role in a
+            company with the opportunity to work with the latest technologies on
+            challenging and diverse projects.
           </p>
           <p align="LEFT">
-            I'm quiet confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.
+            I have experience with the latest backend and front end
+            technologies. I have work experience in both web development and
+            digital marketing
           </p>
           <p>
-            If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
-            photography enthusiast, and tech-obsessed!!!
+            I am proficient in HTML, CSS, SCSS, JavaScript, React.js, Node.js,
+            Python, and SQL
           </p>
         </div>
 
